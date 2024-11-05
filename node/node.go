a5d3ebf7ff2119de88de49bef2a14563581b31fe
karokamp <kamp@itu.dk>
2024-11-05 11:39:52 +0100

    Added field to Node

--- node/node.go
+++ node/node.go
@@ -5,21 +5,17 @@ import (
 	"sync"
 
 	pb "modname/gRPC"
-
 	//"google.golang.org/grpc"
 )
 
-
 type Node struct {
-	pb.ConsensusServer
-	pb.ConsensusClient
-   
-	// Client 
-	nodes      map[string]bool
-	mu                sync.Mutex
-	lamportTime       int64
-	nodeId              int
-	nextNodeId        int
+	pb.UnimplementedConsensusServer                    //server
+	client                          pb.ConsensusClient // client
+	Ports                           []int
+	mu                              sync.Mutex
+	lamportTime                     int64
+	nodeId                          int
+	nextNodeId                      int
 }
 
 func NewNodeServer() *Node {
@@ -31,5 +27,5 @@ func NewNodeServer() *Node {
 //Funktion til SendToken
 
 func (s *Node) Token(ctx context.Context, req *pb.SendToken) (*pb.Void, error) {
-	
+	return pb.Void
 }
